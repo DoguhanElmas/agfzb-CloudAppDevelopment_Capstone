559a008b792fe02c838ed526bc13a22cd512323a
DoguhanElmas <doguhanelmas98@gmail.com>
2021-09-25 11:13:41 +0000

    updates

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -20,7 +20,7 @@ class CarModel(models.Model):
     OTHERS = 'others'
     CAR_CHOICES = [(SEDAN, "Sedan"), (SUV, 'SUV'), (WAGON, 'Wagon'), (OTHERS, 'Others')]
     carmake = models.ForeignKey(CarMake, null= True, on_delete=models.CASCADE)
-    name = models.CharField(null= False, max_length=30, default='Audi X8')
+    name = models.CharField(null= False, max_length=30, default='Ford Focus')
     dealerid = models.IntegerField(null=True)
     cartype = models.CharField(null= False, max_length=20, choices= CAR_CHOICES, default=SEDAN)
     year = models.DateField(null= True)
